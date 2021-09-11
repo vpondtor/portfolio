a3be1545e8310ddb7ea741aa2a4a648e7388382a
vpondtor <virak_pond-tor@brown.edu>
2021-09-11 16:36:55 -0400

    basic skeleton

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 function About(props) {
     return <div id="about">
         <div className="vertical-align content">
-            <p className="intro">Hello! I'm Virak,</p>
+            <p className="intro">Hello! I'm <b>Virak Pond-Tor</b>,</p>
             <p className="intro">a Junior studying Computer Science at Brown University.</p>
         </div>
     </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,18 @@
 function Contact(props) {
     return <div id="contact">
-        <header>Contact</header>
-        <ul>
-            <li>
-                Github
+        <header className="section_title">-- Contact --</header>
+        <ul className="contact-container">
+            <li className="contact-entry">
+                <img src="https://img.icons8.com/material-outlined/45/000000/github.png"/>
             </li>
-            <li>
-                Instagram
+            <li className="contact-entry">
+                <img src="https://img.icons8.com/ios-glyphs/45/000000/instagram.png"/>
+            </li>
+            <li className="contact-entry">
+                <img src="https://img.icons8.com/ios-glyphs/45/000000/linkedin.png"/>
+            </li>
+            <li className="contact-entry">
+            <img src="https://img.icons8.com/material-outlined/45/000000/email.png"/>
             </li>
         </ul>
     </div>
