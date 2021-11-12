3b49bd76fc73de3096fdc70397722d1151496985
vpondtor <71153590+vpondtor@users.noreply.github.com>
2021-11-12 03:13:30 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Featured from './Featured';
 function App() {
   return <div id="app">
     <Intro></Intro>
-    {/* <Projects></Projects> */}
     <Featured></Featured>
     <Contact></Contact>
   </div>;
